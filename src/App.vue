325471ee7b058546d627b30e753b12662066838b
Brian Tambara <btambara@gmail.com>
2024-02-02 23:58:11 -0800

    add font awesome icons, add left side content

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,54 @@ import jobs from './assets/jobs.json'
 </script>
 
 <template>
-  <v-timeline density="compact">
-    <v-timeline-item v-for="job in jobs">
-        <Job
-        :company="job.company"
-        :website="job.website"
-        :location="job.location"
-        :title="job.title"
-        :from="job.from"
-        :to="job.to"
-        :description="job.description"
-        :stack="job.stack"
-      />
-    </v-timeline-item>
-  </v-timeline>
+  <v-container fluid>
+    <v-row>
+      <v-col class="ma-10">
+        <v-row>
+          <h1 class="mb-1 text-left">Brian Tambara</h1>
+          <h2 class="mb-1 text-left">Software Engineer</h2>
+          <p class="text-left">I am a Los Angeles based Software Engineer currently looking for work. My preferred languages for building web applications are Python and Typescript.</p>
+          <v-container>
+              <v-row class="mt-1">
+                <v-btn variant="text">
+                  About
+                </v-btn>
+              </v-row>
+
+              <v-row class="mt-4">
+                <v-btn variant="text">
+                  Resume
+                </v-btn>
+              </v-row>
+          </v-container>
+ 
+          <v-divider :thickness="2" class="mt-4"></v-divider>
+        </v-row>
+        <v-row class="mt-4">
+          <v-btn href="http://github.com/btambara" icon="fab fa-github" variant="plain"></v-btn>
+          <v-btn href="mailto:btambara@gmail.com" icon="fas fa-envelope" variant="plain"></v-btn>
+        </v-row>
+      </v-col>
+      <v-col>
+        <v-timeline density="compact">
+          <v-timeline-item v-for="job in jobs">
+              <Job
+              :company="job.company"
+              :website="job.website"
+              :location="job.location"
+              :title="job.title"
+              :from="job.from"
+              :to="job.to"
+              :description="job.description"
+              :stack="job.stack"
+            />
+          </v-timeline-item>
+        </v-timeline>        
+      </v-col>
+    </v-row>
+  </v-container>
+
+
 
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,24 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// Font Awesome
+import '@fortawesome/fontawesome-free/css/all.css'
+
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/iconsets/fa'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
+    icons: {
+      defaultSet: 'fa',
+      aliases,
+      sets: {
+        fa,
+      },
+    },
     components,
     directives,
   })
