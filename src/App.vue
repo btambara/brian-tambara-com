faad9a7a9d647d7506d97aedbda0994e5ebc6dcc
Brian Tambara <btambara@gmail.com>
2024-02-02 16:38:55 -0800

    remove HelloWorld component, add Job component, add Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Job from './components/Job.vue'
+import jobs from './assets/jobs.json'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-timeline density="compact">
+    <v-timeline-item v-for="job in jobs">
+        <Job
+        :company="job.company"
+        :website="job.website"
+        :location="job.location"
+        :title="job.title"
+        :from="job.from"
+        :to="job.to"
+        :description="job.description"
+        :stack="job.stack"
+      />
+    </v-timeline-item>
+  </v-timeline>
+
 </template>
 
 <style scoped>
---
+++ src/components/Job.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+interface Job {
+    company: string
+    website: string
+    location: string
+    from: string
+    to: string
+    title: string
+    description: string
+    stack: { name: string; }[]
+}
+
+const props = defineProps<Job>()
+</script>
+
+<template>
+    <v-card width="570" :href="props.website" variant="elevated">
+        <v-container fluid>
+            <v-row no-gutters>
+                <v-col class="pt-3 text-right" cols="auto">
+                    <v-card-subtitle>
+                        {{ props.from }} - {{ props.to }}
+                    </v-card-subtitle>
+                </v-col>
+                <v-col class="text-left">      
+                    <v-card-title>
+                        <a :href="props.website">{{ props.company }}</a>
+                    </v-card-title>
+
+                    <v-card-subtitle>{{ props.location }}</v-card-subtitle>
+                    <v-card-subtitle>{{ props.title }}</v-card-subtitle>
+                    <v-card-text>{{ props.description }}</v-card-text>
+                    <div class="ml-2">
+                        <v-chip class="ma-1" v-for="tech in props.stack">{{ tech.name }}</v-chip>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-card>
+</template>
+
+<style scoped></style>
