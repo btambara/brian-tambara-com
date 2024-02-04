92e5a756c0d7ef71cfe8911f3dcefb9dbb33d533
Brian Tambara <btambara@gmail.com>
2024-02-03 22:34:17 -0800

    add functionality to show/hide, add about component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,65 @@
 <script setup lang="ts">
 import Resume from './components/Resume.vue'
+import About from './components/About.vue'
+import { ref } from 'vue';
+
+const isAboutShowing = ref(true)
+const isResumeShowing = ref(false)
+
+function handleAbout(){
+  if(!isAboutShowing.value){
+    isAboutShowing.value = true
+    isResumeShowing.value = false
+  }
+}
+
+function handleResume(){
+  if(!isResumeShowing.value){
+    isAboutShowing.value = false
+    isResumeShowing.value = true
+  }
+}
+
 </script>
 
 <template>
-<Resume />
+    <v-container>
+      <v-row class="h-screen">
+        <v-col class="mt-16" cols="5">
+          <h1 class="mb-1 text-left">Brian Tambara</h1>
+          <h2 class="mb-1 text-left">Software Engineer</h2>
+
+          <p class="text-left">I am a Los Angeles based Software Engineer currently looking for work. My preferred languages for building web applications are Python and Typescript.</p>
+          <v-col class="mt-4">
+            <v-row>
+              <v-btn variant="plain" @click="handleAbout()">
+              About
+              </v-btn>
+            </v-row>
+
+            <v-row>
+              <v-btn variant="plain" @click="handleResume()">
+              Resume
+              </v-btn>
+            </v-row>
+          </v-col>
+
+          <v-btn href="http://github.com/btambara" icon="fab fa-github" variant="text"></v-btn>
+          <v-btn href="mailto:btambara@gmail.com" icon="fas fa-envelope" variant="text"></v-btn>
+        </v-col>
+
+        <v-col>
+          <About v-show="isAboutShowing"/>
+          <Resume v-show="isResumeShowing" />
+        </v-col>
+      </v-row>
+    </v-container>      
 </template>
 
 <style scoped>
+.fixed {
+  position: fixed;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <v-container fluid>
+      <v-row>
+        <v-col>
+            <h2 class="text-left">About Me</h2>
+            <p class="text-left">Quality-focused application development professional with a passion for programming. Dedicated to writing elegant code\r\nthat is reusable, understandable, and easy to maintain. Offer several years of experience in development lifecycle\r\nmanagement. Leverage technical expertise in numerous programming languages and platforms - including [Java, Python, and SQL] - to improve user experiences and create breakthrough, innovative applications.</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+
+<style scoped></style>
